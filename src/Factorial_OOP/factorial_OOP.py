0ceee44207ae347a516ec8d66ff12e2e2290c20a
donatodebattista <doni.db@hotmail.com>
2023-03-25 20:34:59 -0300

    Carga de archivo factorial_OOP

---
+++ src/Factorial_OOP/factorial_OOP.py
@@ -0,0 +1,24 @@
+class Factorial:
+    
+    def __init__(self, min, max):
+        self.min = int(min) 
+        self.max = int(max)
+
+    def run(self):
+        for i in range(self.min, self.max +1):
+            if(i < 0):
+                print(i, "!: El factorial de un número negativo no existe")
+            elif (i==0):
+                print(i, "!: 1")
+            else:
+                fact = 1
+                n = i
+                while(n > 1):
+                    fact *= n
+                    n -= 1
+                print("Factorial de ", i, ": ", fact)
+
+nuevoFactorial = Factorial(-4, 9)
+
+nuevoFactorial.run()
+
