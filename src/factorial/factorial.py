65c027b6aa7bb0426d1e47ded5af7186c6322339
donatodebattista <doni.db@hotmail.com>
2023-03-30 10:48:39 -0300

    Archivo factorial actualizado

--- src/factorial/factorial.py
+++ src/factorial/factorial.py
@@ -21,22 +21,23 @@ def factorial(num):
         return fact 
 
 
-
+#En caso de que no se pasen parametros
 if len (sys.argv) == 1:
-   print("Debe ingresar un numero!")
-   num1 = int(input("Ingrese el número límite: "))
+    print("Debe igresar 2 números!")
+    num1 = int(input("Ingrese el límite 'hasta' (se calculará desde 1 hasta el número indicado): "))
+    num2 = int(input("Ingrese el 'desde' (se calculará desde el numero indicado hasta 60): "))
 
-   for i in range(1, num1+1):
-        if(i < 0):
-            print(factorial(i))
-        else:
-            print("Factorial ",i,"! es ", factorial(i)) 
-
-   sys.exit()
-
-elif len (sys.argv) == 2:
-    num1 = int(sys.argv[1])
+    print("----------------")
+    print("Calculo de factorial para los números entre 1 y", num1)
     for i in range(1, num1+1):
+            if(i < 0):
+                print(factorial(i))
+            else:
+                print("Factorial ",i,"! es ", factorial(i)) 
+
+    print("----------------")
+    print("Calculo de factorial para los números entre", num2, "y 60")
+    for i in range(num2, 61):
         if(i < 0):
             print(factorial(i))
         else:
@@ -44,6 +45,30 @@ elif len (sys.argv) == 2:
 
     sys.exit()
 
+#En caso de que se pasen los 2 numeros como parámetro
+elif len (sys.argv) == 3:
+    num1= int(sys.argv[1])
+    num2= int(sys.argv[2])
 
-#Archivo modificado para solicitar rangos como argumentos
+    print("----------------")
+    print("Calculo de factorial para los números entre 1 y", num1)
+    for i in range(1, num1+1):
+            if(i < 0):
+                print(factorial(i))
+            else:
+                print("Factorial ",i,"! es ", factorial(i)) 
+
+    print("----------------")
+    print("Calculo de factorial para los números entre", num2, "y 60")
+    for i in range(num2, 61):
+        if(i < 0):
+            print(factorial(i))
+        else:
+            print("Factorial ",i,"! es ", factorial(i)) 
+
+    sys.exit()
+    
+# Archivo modificado para:
+#   - solicitar parametro "hasta" y calcular factorial desde 1 hasta el nro indicado.
+#   - Solicitar parametro "desde" y calcular factorial desde el nro indicado hasta 60.
  
