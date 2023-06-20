7792b474c3ebbd30a602b539077c7a000c0a44a8
donatodebattista <doni.db@hotmail.com>
2023-06-20 19:53:29 -0300

    Upgrade to POO

--- src/TP4/RRR_TP1/getJason.py
+++ src/TP4/RRR_TP1/getJason.py
@@ -4,42 +4,46 @@
 # [GCC 4.8.5 20150623 (Red Hat 4.8.5-44)]
 # Embedded file name: getJason.py
 # Compiled at: 2022-06-15 03:15:55
+# coding=utf-8
+
+
 import json, sys
+
+class Singleton():
+    _instancia = None
+
+    def __new__(cls):
+        if cls._instancia == None:
+            cls._instancia = super().__new__(cls)
+        return cls._instancia
+
+class GetJason(Singleton):
+
+    def getHelp(self):
+        print("mensaje de ayuda: \n - Debe ingresar el archivo json como primer argumento\n - Debe ingresar un token como segundo argumento" )
+    def getToken(self, jsf, token):
+        try:
+            with open(jsf, 'r') as (myfile):
+                data = myfile.read()
+                obj = json.loads(data)
+                print (str(obj[token]))
+        except: print("Error: No se ha podido recuperar la clave. Es posible que alguno de los argumentos sean invalidos. Ingrese -h para ayuda.")
+
+
+gj1 = GetJason()
+gj2 = GetJason()
 
 try:
     if(sys.argv[1]):
 
-        #Si el argumento 1 es -h mostrara un mensaje de ayuda 
         if(sys.argv[1] == "-h"):  
-            print("mensaje de ayuda: \n - Debe ingresar el archivo json como primer argumento\n - Debe ingresar un token como segundo argumento" )
-        
+            gj1.getHelp()
+
         else:
             try:                
                 if (sys.argv[2]):
-                    
-                    jsonfile = sys.argv[1]
-                    jsonkey = sys.argv[2]
-                    try:
-                        with open(jsonfile, 'r') as (myfile):
-                            data = myfile.read()
-                        obj = json.loads(data)
-                        print (str(obj[jsonkey]))
-                    except: print("Error: No se ha podido recuperar la clave. Es posible que alguno de los argumentos sean invalidos. Ingrese -h para ayuda.")
+                    gj1.getToken(sys.argv[1], sys.argv[2])
             except:
-                print("Error: Segundo argumento inexistente. Ingrese -h para ayuda.")
+                print("Error: Segundo argumento inextistente. Ingrese -h para ayuda.")
 except:
-        print("Error: Pirmer argumento inexistente. Ingrese -h para ayuda.")
-
-
-
-
-
-""" 
-!-----Programa antiguo------!    
-
-jsonfile = sys.argv[1]
-jsonkey = 
-with open(jsonfile, 'r') as (myfile):
-    data = myfile.read()
-obj = json.loads(data)
-print str(obj[jsonkey]) """
+    print("Error: Pirmer argumento inexistente. Ingrese -h para ayuda.")
