52b17df2b228c7d534809509bb9345ab7a7a6c71
donatodebattista <doni.db@hotmail.com>
2023-06-21 22:17:36 -0300

    chain of responsibility automation

--- src/TP4/RRR_TP1/getJason.py
+++ src/TP4/RRR_TP1/getJason.py
@@ -1,66 +1,107 @@
-""" Este modulo permite recuperar clave de acceso API a 
-    microservicios contenidos en un archivo JSON 
-"""
-
-# uncompyle6 version 3.5.0
-# Python bytecode 2.7 (62211)
-# Decompiled from: Python 2.7.5 (default, Nov 16 2020, 22:23:17)
-# [GCC 4.8.5 20150623 (Red Hat 4.8.5-44)]
-# Embedded file name: getJason.py
-# Compiled at: 2022-06-15 03:15:55
-# coding=utf-8
-
-#copyright IS2 2022,2023 todos los derechos reservados
-
 import json
 import sys
 
-class Singleton():
-    """ Declara un metodo que devuelve la misma instancia de su propia clase """
-    _instancia = None
+class Handler(object):
+    next_handler = None
 
-    def __new__(cls):
-        if cls._instancia is None:
-            cls._instancia = super().__new__(cls)
-        return cls._instancia
 
-class GetJson(Singleton):
-    """ Clase que permite recuperar las Api tokens """
-    def get_help(self):
-        """ Muestra un mensaje de ayuda en pantalla """
-        print("mensaje de ayuda:")
-        print("- Debe ingresar el archivo json como primer argumento")
-        print("- Debe ingresar un token como segundo argumento")
+    def __init__(self):
+        self.next_Handler = None
 
-    def get_token(self, jsf, token):
-        """ Muestra en pantalla la clave de acceso API correspondiente al token ingresado """
-        try:
-            with open(jsf, 'r') as (my_file):
-                data = my_file.read()
-                obj = json.loads(data)
-                print (str(obj[token]))
-        except:
-            print("Error: No se ha podido recuperar la clave. ")
-            print("Es posible que alguno de los argumentos sean invalidos. Ingrese -h para ayuda.")
+    def set_next(self, handler):
+        self.next_handler = handler
+        return handler
+    
+    def handle(self, request):
+        if self.next_handler == None:
+           print("Ninguna cuenta pudo realizar el pago")
+           return
+        self.next_handler.handle(request)
 
-#-----------MAIN-----------#
-gj1 = GetJson()
-gj2 = GetJson()
 
-# El control de los argumentos se realiza previo al llamado de los metodos
-try:
-    if sys.argv[1]:
 
-        # Si el primer argumento es -h se invoca al metodo getHelp del objeto GetJason
-        if sys.argv[1] == "-h":
-            gj1.get_help()
+
+class Cuenta1Handler(Handler):
+
+    def __init__(self, saldo):
+        self.saldo = saldo
+
+    def handle(self, monto_a_pagar):
+        print("Intentando realizar pago con cuenta 1")
+
+        if self.saldo >= monto_a_pagar:
+            self.saldo = self.saldo - monto_a_pagar
+
+            try:
+                with open("./sitedata.json", 'r') as (my_file):
+                    data = my_file.read()
+                    obj = json.loads(data)
+                    print ("token", str(obj["token1"]))
+                    print("pago realizado con cuenta 1") 
+                    return monto_a_pagar
+
+            except:
+                print("Error: No se ha podido recuperar la clave. ")
+        else:
+            print("Cuenta 1 no pudo pagar")
+            super(Cuenta1Handler, self).handle(monto_a_pagar)
+
+class Cuenta2Handler(Handler):
+
+    def __init__(self, saldo):
+        self.saldo = saldo
+
+    def handle(self, monto_a_pagar):
+        print("Intentando realizar pago con cuenta 2")
+        if self.saldo >= monto_a_pagar:
+            self.saldo = self.saldo - monto_a_pagar
+            try:
+                with open("./sitedata.json", 'r') as (my_file):
+                    data = my_file.read()
+                    obj = json.loads(data)
+                    print("pago realizado con cuenta 2") 
+                    print ("token:", str(obj["token2"]))
+                    return monto_a_pagar
+
+            except:
+                print("Error: No se ha podido recuperar la clave. ")
 
         else:
-            try:
-                if sys.argv[2]:
-                    # Si existe un segundo argumento se invoca al metodo getToken
-                    gj1.get_token(sys.argv[1], sys.argv[2])
-            except:
-                print("Error: Segundo argumento inextistente. Ingrese -h para ayuda.")
-except:
-    print("Error: Pirmer argumento inexistente. Ingrese -h para ayuda.")
+            print("Cuenta 2 no pudo pagar")
+            super(Cuenta2Handler, self).handle(monto_a_pagar)
+
+
+        
+class pedido_pago():
+    
+    def __init__(self, monto, nro_pago):
+        self.monto = monto
+        # El nro de pago se pasa como atributo por practicidad del ejercicio
+        self.nro_pago = nro_pago
+
+
+
+
+#!-----------MAIN-----------#
+cuenta1 = Cuenta1Handler(1000)
+cuenta2 = Cuenta2Handler(2000)
+cuenta1.set_next(cuenta2)
+
+pago1 = pedido_pago(500, 1)
+pago2 = pedido_pago(1000, 2)
+pago3 = pedido_pago(500, 3)
+pago4 = pedido_pago(2000, 4)
+
+cuenta1.handle(pago1.monto)
+cuenta1.handle(pago2.monto)
+cuenta1.handle(pago3.monto)
+cuenta1.handle(pago4.monto)
+
+historial_pagos = []
+
+historial_pagos.append(cuenta1.handle(pago1.monto))
+historial_pagos.append(cuenta1.handle(pago2.monto))
+historial_pagos.append(cuenta1.handle(pago3.monto))
+historial_pagos.append(cuenta1.handle(pago4.monto))
+
+print(historial_pagos)
